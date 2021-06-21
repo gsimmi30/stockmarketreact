ccec4479ae477d7ab9ab3f6db3e4d9b3b007d66e
Simmi Gangwar <66629461+gsimmi30@users.noreply.github.com>
2021-06-21 11:30:45 +0530

    Add files via upload

--- src/http-common.js
+++ src/http-common.js
@@ -2,9 +2,9 @@ import axios from "axios";
 
 export default axios.create({
   method:'OPTIONS, PUT, POST, GET, DELETE',
-  baseURL: "http://localhost:8080",
+  baseURL: "https://reactappstock.herokuapp.com/",
   headers: { 'Content-Type': 'application/json',
-  'Access-Control-Allow-Origin' : 'http://192.168.1.5:3000',
+  'Access-Control-Allow-Origin' : '*',
   'Accept': 'application/json',
   'Vary':'Origin'
  }
