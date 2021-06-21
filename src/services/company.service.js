6065f84b8ff196fd04c41a82479e3d75d2cf4c5c
Simmi Gangwar <66629461+gsimmi30@users.noreply.github.com>
2021-06-21 12:55:25 +0530

    Add files via upload

--- src/services/company.service.js
+++ src/services/company.service.js
@@ -3,35 +3,35 @@ import http from "../http-common";
 class CompanyService {
 
   findAll() {
-    return http.get(`/companies`);
+    return http.get(`companies`);
   }
 
   findById(id) {
-    return http.get(`/companies/${id}`);
+    return http.get(`companies/${id}`);
   }
 
   create(data) {
-    return http.post(`/companies`, data);
+    return http.post(`companies`, data);
   }
 
   update(data) {
-    return http.put(`/companies`, data);
+    return http.put(`companies`, data);
   }
 
   delete(id) {
-    return http.delete(`/companies/${id}`);
+    return http.delete(`companies/${id}`);
   }
 
   findIpos(id) {
-    return http.get(`/companyipos/${id}`);
+    return http.get(`companyipos/${id}`);
   }
 
   findStockPrices(id) {
-    return http.get(`/companystockprices/${id}`);
+    return http.get(`companystockprices/${id}`);
   }
 
   findByTitle(pattern){
-    return http.get(`/companiesmatch/${pattern}`);}
+    return http.get(`companiesmatch/${pattern}`);}
 }
 
 export default new CompanyService();
