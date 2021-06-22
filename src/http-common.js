5a1bf5c513c6a02908b610d6c97d5515de8be682
Simmi Gangwar <66629461+gsimmi30@users.noreply.github.com>
2021-06-22 21:24:00 +0530

    Add files via upload

--- src/http-common.js
+++ src/http-common.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export default axios.create({
   method:'OPTIONS, PUT, POST, GET, DELETE',
-  baseURL: "http://localhost:8080",
+  baseURL: "https://ancient-shore-52392.herokuapp.com",
   headers: { 'Content-Type': 'application/json',
   'Access-Control-Allow-Origin' : '*',
   'Accept': 'application/json',
